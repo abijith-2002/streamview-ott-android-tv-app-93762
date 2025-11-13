e3766205b1fb92953ed63dbbbcfd160b906380af
Kavia AI <production@kavia.ai>
2025-11-13 06:09:07 +0000

    Checkpoint 13 - cg9d92baf7

--- streamly_frontend/app/src/main/java/com/example/streamly_frontend/home/HomeSectionsAdapter.kt
+++ streamly_frontend/app/src/main/java/com/example/streamly_frontend/home/HomeSectionsAdapter.kt
@@ -346,8 +346,8 @@ class HomeSectionsAdapter(
 
                         val density = (itemView.resources?.displayMetrics?.density ?: 1f)
                         val headerDp = 52f
-                        val marginDp = 12f
-                        val railsPadTopDp = 6f
+                        val marginDp = 24f
+                        val railsPadTopDp = 24f
                         val safetyDp = 8f
                         val extraTopPx = ((headerDp + marginDp + railsPadTopDp + safetyDp) * density).toInt()
 
